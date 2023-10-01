070e5029acce40adb156ddd00086114238cd056b
almer.maulana <almer.maulana1234@gmail.com>
2023-10-01 08:20:15 +0700

    Change Sigarang to Sibapokting

--- mtp2023/urls.py
+++ mtp2023/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-admin.site.site_header = 'Admin SiGarang'
-admin.site.site_title = 'Admin SiGarang'
+admin.site.site_header = 'Admin SIBAPOKTING'
+admin.site.site_title = 'Admin SIBAPOKTING'
