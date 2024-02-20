4d904b8a8e567f32698a9870efb260d79f119041
almer06 <almer.maulana1234@gmail.com>
2024-02-20 20:43:37 +0700

    add Harga Kemarin in add form

--- administrator/forms.py
+++ administrator/forms.py
@@ -1,7 +1,21 @@
+from datetime import timedelta, date
+
 from django import forms
+
+from administrator.models import UnitGroceries
 from visitor.models import User
 
 
 class LoginForm(forms.ModelForm):
     class Meta:
         model = User
+        fields = '__all__'
+
+
+class UnitGroceriesForm(forms.ModelForm):
+    price_yesterday = forms.IntegerField(label='Harga Kemarin', min_value=0, initial=0,
+                                         widget=forms.TextInput(attrs={'disabled': 'true'}))
+
+    class Meta:
+        model = UnitGroceries
+        fields = ['unit_groceries_variant', 'price_yesterday', 'unit_groceries_price', 'unit_groceries_created']
--- administrator/views.py
+++ administrator/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from datetime import datetime, timedelta
+
 from django.contrib.auth.views import LoginView, TemplateView
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+
+from administrator.models import UnitGroceries
 
 
 # Create your views here.
@@ -12,3 +17,22 @@ class DashboardView(TemplateView):
     extra_context = {
         'title': 'Dashboard'
     }
+
+
+def get_price_sembako_yesterday(request):
+    id_sembako = request.GET.get('id_sembako')
+
+    tanggal_hari_ini = datetime.now().date()
+    tanggal_kemarin = tanggal_hari_ini - timedelta(days=1)
+
+    query = UnitGroceries.objects.filter(unit_groceries_variant__groceries_id=id_sembako,
+                                         unit_groceries_created=tanggal_kemarin)
+
+    if query.count() == 0:
+        return JsonResponse({
+            'price': 0
+        })
+
+    return JsonResponse({
+        'price': query.first().unit_groceries_price
+    })
